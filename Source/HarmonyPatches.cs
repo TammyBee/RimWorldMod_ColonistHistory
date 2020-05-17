2bf56596a5568ba4799ed65b321c4397511169e1
TammyBee <tammybeetmb@gmail.com>
2020-05-17 13:13:22 +0900

    add legend highlight.

---
+++ Source/HarmonyPatches.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+using Verse;
+
+namespace ColonistHistory {
+    [StaticConstructorOnStartup]
+    public class HarmonyPatches {
+        static HarmonyPatches() {
+            var harmony = new Harmony("com.tammybee.colonisthistory");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(SimpleCurveDrawer))]
+    [HarmonyPatch("DrawCurveMousePoint")]
+    public class SimpleCurveDrawer_DrawCurveMousePoint_Patch {
+        public static Rect screenRect;
+        public static Rect viewRect;
+
+        static void Prefix(Rect screenRect, Rect viewRect) {
+            SimpleCurveDrawer_DrawCurveMousePoint_Patch.screenRect = screenRect;
+            SimpleCurveDrawer_DrawCurveMousePoint_Patch.viewRect = viewRect;
+        }
+    }
+}
--- Source/RecordGroup.cs
+++ Source/RecordGroup.cs
@@ -85,15 +85,87 @@ namespace ColonistHistory {
 			Text.Anchor = TextAnchor.UpperLeft;
 		}
 
+		private static List<SimpleCurveDrawInfo> GetFocusedCurves(List<SimpleCurveDrawInfo> curves, Rect screenRect, Rect viewRect) {
+			if (curves.Count == 0) {
+				return null;
+			}
+			if (!Mouse.IsOver(screenRect)) {
+				return null;
+			}
+			GUI.BeginGroup(screenRect);
+			Vector2 mousePosition = Event.current.mousePosition;
+			Vector2 vector = default(Vector2);
+			Vector2 vector2 = default(Vector2);
+			bool flag = false;
+			List<SimpleCurveDrawInfo> simpleCurveDrawInfos = new List<SimpleCurveDrawInfo>();
+			foreach (SimpleCurveDrawInfo simpleCurveDrawInfo2 in curves) {
+				if (simpleCurveDrawInfo2.curve.PointsCount != 0) {
+					Vector2 vector3 = SimpleCurveDrawer.ScreenToCurveCoords(screenRect, viewRect, mousePosition);
+					vector3.y = simpleCurveDrawInfo2.curve.Evaluate(vector3.x);
+					Vector2 vector4 = SimpleCurveDrawer.CurveToScreenCoordsInsideScreenRect(screenRect, viewRect, vector3);
+					if (!flag || Mathf.Approximately(Vector2.Distance(vector4, mousePosition), Vector2.Distance(vector2, mousePosition))) {
+						flag = true;
+						vector = vector3;
+						vector2 = vector4;
+						simpleCurveDrawInfos.Add(simpleCurveDrawInfo2);
+					}else if (!flag || Vector2.Distance(vector4, mousePosition) < Vector2.Distance(vector2, mousePosition)) {
+						flag = true;
+						vector = vector3;
+						vector2 = vector4;
+						simpleCurveDrawInfos.Clear();
+						simpleCurveDrawInfos.Add(simpleCurveDrawInfo2);
+					}
+				}
+			}
+			GUI.EndGroup();
+			return simpleCurveDrawInfos;
+		}
+
 		private void DrawCurvesLegend(Rect rect, List<SimpleCurveDrawInfo> curves) {
+			List<SimpleCurveDrawInfo> focusedCurves = GetFocusedCurves(curves, SimpleCurveDrawer_DrawCurveMousePoint_Patch.screenRect, SimpleCurveDrawer_DrawCurveMousePoint_Patch.viewRect);
 			float newWidth = rect.width - GUI.skin.verticalScrollbar.fixedWidth - 2f;
 			int columnCount = (int)(newWidth / 140f);
 			int rowCount = curves.Count / columnCount + 1;
 			float newHeight = rowCount * 20f;
 			Rect newRect = new Rect(rect.x, rect.y, newWidth, newHeight);
 			Widgets.BeginScrollView(rect, ref scrollPosition, newRect);
-			SimpleCurveDrawer.DrawCurvesLegend(newRect, curves);
+			RecordGroup.DrawCurvesLegendInternal(newRect, curves, focusedCurves);
 			Widgets.EndScrollView();
+		}
+
+		private static void DrawCurvesLegendInternal(Rect rect, List<SimpleCurveDrawInfo> curves, List<SimpleCurveDrawInfo> focusedCurves) {
+			Text.Anchor = TextAnchor.UpperLeft;
+			Text.Font = GameFont.Small;
+			Text.WordWrap = false;
+			GUI.BeginGroup(rect);
+			float num = 0f;
+			float num2 = 0f;
+			int num3 = (int)(rect.width / 140f);
+			int num4 = 0;
+			foreach (SimpleCurveDrawInfo simpleCurveDrawInfo in curves) {
+				GUI.color = simpleCurveDrawInfo.color;
+				GUI.DrawTexture(new Rect(num, num2 + 2f, 15f, 15f), BaseContent.WhiteTex);
+				GUI.color = Color.white;
+				num += 20f;
+				if (simpleCurveDrawInfo.label != null) {
+					Rect rectLabel = new Rect(num, num2, 140f, 20f);
+					Widgets.Label(rectLabel, simpleCurveDrawInfo.label);
+					if (!focusedCurves.NullOrEmpty() && focusedCurves.Exists(c => simpleCurveDrawInfo.label == c.label && simpleCurveDrawInfo.color == c.color)) {
+						Widgets.DrawHighlight(rectLabel);
+					}
+				}
+				num4++;
+				if (num4 == num3) {
+					num4 = 0;
+					num = 0f;
+					num2 += 20f;
+				} else {
+					num += 140f;
+				}
+			}
+			GUI.EndGroup();
+			GUI.color = Color.white;
+			Text.WordWrap = true;
 		}
 	}
 }
